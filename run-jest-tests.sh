8706e4241f43d535ea3621a5420ef238713b33df
mike dupont <mike.dupont@introspector.local>
2024-09-16 16:27:05 -0400

    run jest

--- run-jest-tests.sh
+++ run-jest-tests.sh
@@ -5,6 +5,6 @@ shopt -s globstar # to expand '**' into nested directories
 
 for f in ./src/**/*.test.ts; do
     #NODE_OPTIONS=--experimental-vm-modules npx jest $f;
-    NODE_OPTIONS=--experimental-vm-modules  NO_INSIGHT=true clinic flame -- node ./node_modules/.bin/jest $f
-    NODE_OPTIONS=--experimental-vm-modules  NO_INSIGHT=true clinic doctor -- node ./node_modules/.bin/jest $f
+    NODE_OPTIONS=--experimental-vm-modules  NO_INSIGHT=true clinic flame -- node ./node_modules/jest-cli/bin/jest.js $f
+    NODE_OPTIONS=--experimental-vm-modules  NO_INSIGHT=true clinic doctor -- node ./node_modules/jest-cli/bin/jest.js $f
 done
