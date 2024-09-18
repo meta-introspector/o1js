917db09df5e8598946f621357465bc7f3f7db13f
mike dupont <mike.dupont@introspector.local>
2024-09-18 08:31:52 -0400

    update version of clinic in jest

--- README-dev.md
+++ README-dev.md
@@ -388,3 +388,4 @@ Using docker hub create a repo to hold the data.
 ## 
 `docker run -v /mnt/data1/:/mnt/data1 -it h4ckermike/o1js-clinicjs-data:sept-17-2024 bash`
 `cp -r .clinic /mnt/data1/nix/time/2024/09/17/o1js/_clinic`
+`sudo chown -R mdupont: _clinic `
--- run-all-tests.sh
+++ run-all-tests.sh
@@ -7,7 +7,19 @@ set +e
 
 #######
 find /app > /tmp/app-files-list.txt
-NO_INSIGHT=true clinic doctor --collect-only -- node /usr/local/bin/pnpm run test
+#NO_INSIGHT=true clinic doctor --collect-only --
+node /usr/local/bin/pnpm run test
+#./run-jest-tests.sh
+export NODE_OPTIONS=--experimental-vm-modules
+export NO_INSIGHT=true
+clinic doctor --collect-only -- node ./node_modules/jest-cli/bin/jest.js ./src/mina-signer/tests/rosetta.test.ts
+clinic doctor --collect-only -- node ./node_modules/.pnpm/@jest+monorepo@https+++codeload.github.com+meta-introspector+jest+tar.gz+4a58402556ecd05ca9cc01873db6fbc5596ccc67/node_modules/@jest/monorepo/packages/jest-cli/bin/jest.js ./src/mina-signer/tests/rosetta.test.ts
+
+# + for f in ./src/**/*.test.ts
+#./src/lib/mina/precondition.test.ts
+#./src/lib/mina/token.test.ts
+#./src/lib/provable/test/primitives.test.ts
+
 NO_INSIGHT=true clinic doctor --collect-only -- node /usr/local/bin/pnpm run test:integration
 NO_INSIGHT=true clinic doctor --collect-only -- node /usr/local/bin/pnpm run test:unit
 NO_INSIGHT=true clinic doctor --collect-only -- node /usr/local/bin/pnpm run test:e2e
