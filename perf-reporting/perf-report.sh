4b1a2ba77525f87196f7f61d4290b545bbe66cba
mike dupont <mike.dupont@introspector.local>
2024-09-21 14:27:37 -0400

    using different directories for output

--- perf-reporting/perf-report.sh
+++ perf-reporting/perf-report.sh
@@ -9,38 +9,44 @@
 ls -latr /app/perf-reporting/input_data/ 
 find /app/perf-reporting/input_data/
 
-cd /app/perf-reporting/output
+pushd /app/perf-reporting/output || exit
 
 if [ -f /app/perf-reporting/scripts/perf-report.sh ] ;
 then
    bash -x /app/perf-reporting/scripts/perf-report.sh
 fi
 
-if [ ! -f  perf.data ]
-then
-    unzip -u /app/perf-reporting/input_data/perf.data.zip
-    tar -xzf perf.data.tar.gz
-    ls -latr
-    find .
-    #tar xvf src/lib/provable/test/primitives.test.ts.perf.data.tar.bz2 -C ~/.debug
-#    unzip /app/perf-reporting/input_data/perf.data.zip
-fi
+mkdir -p /app/perf-reporting/output/unzip
+mkdir -p /app/perf-reporting/output/untar
 
-ls -latr /app/perf-reporting/output/tmp/perf/
+for zipf in /app/perf-reporting/input_data/*.zip;
+do
+    echo "${zipf}"
+    unzip -u "${zipf}" -d /app/perf-reporting/output/unzip
+done
+
+for tarf in /app/perf-reporting/output/unzip/*.tar.gz;
+do
+    echo "${tarf}"
+    tar -xzf "${tarf}" -C /app/perf-reporting/output/untar/
+    ls -later /app/perf-reporting/output/untar/
+    find /app/perf-reporting/output/untar/
+done
+
+ls -latr /app/perf-reporting/output/untar/tmp/perf/
 
 mkdir -p ~/.debug
 
-for perfdata in /app/perf-reporting/output/tmp/perf/*.perf.data;
+for perfdata in /app/perf-reporting/output/untar/tmp/perf/*.perf.data;
 do
-    echo $perfdata
-    ls -latr ${perfdata}
+    echo "${perfdata}"
+    ls -latr "${perfdata}"
     tar xvf "${perfdata}.tar.bz2" -C ~/.debug		
-    perf report -i $perfdata --verbose --stdio --header > ${perfdata}.header.txt 2>&1
-    perf report -i $perfdata --verbose --stdio --stats > ${perfdata}.stats.txt 2>&1
-    perf script -F +pid -i $perfdata  > ${perfdata}.script.txt 2>&1
-    perf report --stdio -i $perfdata  > ${perfdata}.report.txt 2>&1
-    ls -latr ${perfdata}
+    perf report -i "${perfdata}" --verbose --stdio --header > "${perfdata}.header.txt" 2>&1
+    perf report -i "${perfdata}" --verbose --stdio --stats > "${perfdata}.stats.txt" 2>&1
+    perf script -F +pid -i "${perfdata}"  > "${perfdata}.script.txt" 2>&1
+    perf report --stdio -i "${perfdata}"  > "${perfdata}.report.txt" 2>&1
+    ls -latr "${perfdata}"
 done
 
-#ls -latr /app/perf-reporting/data/
-#find /app/perf-reporting/data/
+ls -latr /app/perf-reporting/output/untar/tmp/perf/
