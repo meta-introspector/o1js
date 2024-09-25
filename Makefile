6ecb4b9d599c7eb029dc8618a67ef942da778bdd
mike dupont <mike.dupont@introspector.local>
2024-09-25 12:13:44 -0400

    Just test
    now to test on github
    
    Update run-docker-tests.yml
    
    update documentation
    
    fix syntax
    
    install clinic
    
    Update run-docker-tests.yml
    
    adding verbose
    
    on push as well
    
    on push as well

--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 test:
 	docker compose build mina-local-network
+<<<<<<< HEAD
 	docker compose up mina-local-network
+=======
+	docker compose up mina-local-network 
+>>>>>>> db5a825c8 (Just test)
--- run-all-tests.sh
+++ run-all-tests.sh
@@ -1,72 +1,74 @@
-
+#!/bin/bash
 set +e
+set -x
+export OUTPUT_DIR="/tmp/perf"
+export NODE_OUTPUT_DIR="${OUTPUT_DIR}/node"
+export CLINIC_OUTPUT_DIR="${OUTPUT_DIR}/clinic"
+export NO_INSIGHT=true
+export NODE_OPTIONS="--experimental-vm-modules --perf-basic-prof --enable-source-maps --stack-trace-limit=1000 --report-dir $NODE_OUTPUT_DIR    --perf-prof-unwinding-info --perf-prof"
+export NO_INSIGHT=true
+export JESTOPTS=--collectCoverage
+export NODEOPT1="--prof  --expose-gc"
 
-#NO_INSIGHT=true clinic doctor -- node --perf-basic-prof --enable-source-maps --stack-trace-limit=1000 ./node_modules/jest-cli/bin/jest.js
-#NO_INSIGHT=true clinic flame -- node --perf-basic-prof --enable-source-maps --stack-trace-limit=1000 ./node_modules/jest-cli/bin/jest.js
-# May Clinic.js report anonymous usage statistics to improve the tool over time?
 
-#######
-#find /app > /tmp/app-files-list.txt
-#NO_INSIGHT=true clinic doctor --collect-only --
-#node /usr/local/bin/pnpm run test
-#./run-jest-tests.sh
-export NODE_OPTIONS=--experimental-vm-modules
-#export NO_INSIGHT=true
-#npx jest
-#clinic doctor -- node ./node_modules/.bin/../jest/bin/jest.js src/lib/provable/test/int.test.ts
+# setup perf permissions
 echo 2 > /proc/sys/kernel/perf_event_paranoid
 
+mkdir "${OUTPUT_DIR}"
+mkdir "${NODE_OUTPUT_DIR}"
+mkdir "${CLINIC_OUTPUT_DIR}"
+pnpm install -g clinic
 
-
-# mdupont@mdupont-G470:~/2024/09/20/o1js$ grep PASS report1.txt
-# PASS src/lib/provable/test/merkle-list.test.ts (7.118 s)
-# PASS src/lib/provable/test/merkle-tree.test.ts (7.297 s)
-# PASS src/lib/provable/test/scalar.test.ts (7.623 s)
-# PASS src/lib/provable/test/merkle-map.test.ts (7.353 s)
-# PASS src/lib/provable/test/provable.test.ts (8.322 s)
-# PASS src/lib/provable/test/primitives.test.ts (8.346 s)
-# PASS src/lib/provable/test/group.test.ts (8.371 s)
-# PASS src/lib/provable/test/int.test.ts (8.64 s)
-# PASS src/lib/mina/precondition.test.ts (12.688 s)
-# PASS src/lib/mina/token.test.ts (46.593 s)
-
-mkdir /tmp/perf/
+# limit to only working tests
 TESTS="src/lib/provable/test/merkle-list.test.ts src/lib/provable/test/merkle-tree.test.ts src/lib/provable/test/scalar.test.ts  src/lib/provable/test/merkle-map.test.ts  src/lib/provable/test/provable.test.ts  src/lib/provable/test/primitives.test.ts  src/lib/provable/test/group.test.ts  src/lib/provable/test/int.test.ts  src/lib/mina/precondition.test.ts src/lib/mina/token.test.ts"
-for testname in $TESTS;
+# FIXME this all tests is not used because many of them fail.
+ALL_TESTS=`ls -b src/lib/provable/test/*.test.ts src/lib/mina/*.test.ts `
 
-do
-    perfdata=${testname}.perf.data
-    clinic flame -- node --perf-basic-prof ./node_modules/.bin/../jest/bin/jest.js ${testname}
-    clinic doctor -- node --perf-basic-prof ./node_modules/.bin/../jest/bin/jest.js ${testname}
-    clinic bubbleprof -- node --perf-basic-prof ./node_modules/.bin/../jest/bin/jest.js ${testname}
-    clinic heapprofiler -- node --perf-basic-prof ./node_modules/.bin/../jest/bin/jest.js ${testname}
-    
-    perf record -g -o ${testname}.perf.data -F 999 --call-graph dwarf node --perf-basic-prof ./node_modules/.bin/../jest/bin/jest.js ${testname} > ${testname}.reportout.txt 2>&1
+
+run_test() {
+    testname=$1
+    export MULTIPLE="${testname}"
+    export perfdata="${testname}.perf.data"
+    OUTPUT_DIR2="${OUTPUT_DIR}/$testname/"
+    mkdir -p "${OUTPUT_DIR2}clinic/"
+    mkdir -p "${OUTPUT_DIR2}log/"
+    mkdir -p "${OUTPUT_DIR2}coverage/"
+
+    clinic flame -- node $NODEOPT1 ./node_modules/.bin/../jest/bin/jest.js "${JESTOPTS}" "${MULTIPLE}" | tee "${OUTPUT_DIR2}clinic-flame.txt" 2>&1
+    clinic doctor -- node $NODEOPT1 ./node_modules/.bin/../jest/bin/jest.js "${JESTOPTS}" "${MULTIPLE}" | tee "${OUTPUT_DIR2}clinic-doctor.txt" 2>&1
+    clinic bubbleprof -- node $NODEOPT1 ./node_modules/.bin/../jest/bin/jest.js "${JESTOPTS}" "${MULTIPLE}" | tee "${OUTPUT_DIR2}clinic-bubble.txt" 2>&1
+    clinic heapprofiler -- node $NODEOPT1 ./node_modules/.bin/../jest/bin/jest.js "${JESTOPTS}" "${MULTIPLE}"  | tee "${OUTPUT_DIR2}clinic-heap.txt" 2>&1    
+    perf record -g -o "${testname}.perf.data" -F 999 --call-graph dwarf node  $NODEOPT1 ./node_modules/.bin/../jest/bin/jest.js "${JESTOPTS}" "${MULTIPLE}" > "${testname}.reportout.txt" 2>&1
     perf archive ${testname}.perf.data
     perf report -i "${perfdata}" --verbose --stdio --header > "${perfdata}.header.txt" 2>&1
     perf report -i "${perfdata}" --verbose --stdio --stats > "${perfdata}.stats.txt" 2>&1
     perf script -F +pid -i "${perfdata}"  > "${perfdata}.script.txt" 2>&1
     perf report --stdio -i "${perfdata}"  > "${perfdata}.report.txt" 2>&1
+    ls -latr jit*.dump
+    rm -r jit*.dump
+    
+    mv ${testname}.perf.data "${OUTPUT_DIR2}/perf_data"
+    mv "${testname}.*.txt" "${OUTPUT_DIR2}/perf_data"
+    mv .clinic/* "${OUTPUT_DIR2}clinic/"
+    mv coverage/* "${OUTPUT_DIR2}coverage/"
+    mv  *.log "${OUTPUT_DIR2}log/"    
+    du -s "${OUTPUT_DIR2}"
+    find "${OUTPUT_DIR2}"
+    ls -latr "${OUTPUT_DIR2}"
+}
 
-    cp ${testname}.* /tmp/perf/
-    mkdir /tmp/perf/clinic
-    cp -r .clinic/* /tmp/perf/clinic
+for testname in $TESTS;
+do
+    results=$(run_test $testname)
+    echo $results
 done
 
 tar -czf /tmp/perf.data.tar.gz /tmp/perf/*
-#perf script > perf_script.txt
-#perf report > perf_report.txt
-#clinic doctor --collect-only -- node ./node_modules/jest-cli/bin/jest.js ./src/mina-signer/tests/rosetta.test.ts
-#clinic doctor --collect-only -- node ./node_modules/.pnpm/@jest+monorepo@https+++codeload.github.com+meta-introspector+jest+tar.gz+4a58402556ecd05ca9cc01873db6fbc5596ccc67/node_modules/@jest/monorepo/packages/jest-cli/bin/jest.js ./src/mina-signer/tests/rosetta.test.ts
 
 # + for f in ./src/**/*.test.ts
 #./src/lib/mina/precondition.test.ts
 #./src/lib/mina/token.test.ts
 #./src/lib/provable/test/primitives.test.ts
-
-# NO_INSIGHT=true clinic doctor --collect-only -- node /usr/local/bin/pnpm run test:integration
-# NO_INSIGHT=true clinic doctor --collect-only -- node /usr/local/bin/pnpm run test:unit
-# NO_INSIGHT=true clinic doctor --collect-only -- node /usr/local/bin/pnpm run test:e2e
 
 # #######
 # NO_INSIGHT=true clinic flame -- node /usr/local/bin/pnpm run test
@@ -74,9 +76,3 @@ tar -czf /tmp/perf.data.tar.gz /tmp/perf/*
 # NO_INSIGHT=true clinic flame -- node /usr/local/bin/pnpm run test:unit
 # NO_INSIGHT=true clinic flame -- node /usr/local/bin/pnpm run test:e2e
 
-# #clinic flame -- node index.js
-
-# # pnpm run test
-# # pnpm run test:integration
-# # pnpm run test:unit
-# # pnpm run test:e2e
