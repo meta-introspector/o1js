aa3f77f8862b9bca6cdfbd18014e890ffa23b5e1
mike dupont <mike.dupont@introspector.local>
2024-09-28 16:53:30 -0400

    o1js

--- run-all-unit-tests-local.sh
+++ run-all-unit-tests-local.sh
@@ -1,4 +1,9 @@
 #find /app
+corepack enable
+corepack prepare pnpm@latest-9 --activate
+pnpm install
+pnpm install -g clinic
+pnpm install -g npm@10.8.3
 
 npx tsc -p tsconfig.test.json
 pnpm run build
